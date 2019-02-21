745d6a9d6a92f354fb0aba9bd89441269ca3eebf
Irina Alina Gabriela Luca <iagl@netcompany.com>
2019-02-21 22:56:15 +0100

    before Elisabeth merge

--- RoutePlanningCES/RoutePlanningCES/Controllers/SearchResultController.cs
+++ RoutePlanningCES/RoutePlanningCES/Controllers/SearchResultController.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using RoutePlanningCES.Models.DTOs;
 
 namespace RoutePlanningCES.Controllers
 {
     public class SearchResultController : Controller
     {
-        public ActionResult SearchResult()
+        public ActionResult SearchResult(int SourceId, int DestinationId, List<int> ParcelTypes, int weight, int width, int height, int length)
         {
-            return View();
+            var result = new SearchResultDTO();
+            return View(result);
         }
     }
 }

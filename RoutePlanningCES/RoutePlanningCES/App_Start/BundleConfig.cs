2fe6691df8e4ca6cabc8a12e61c0de6e6961f222
Elisabeth Marie Alice Pietras <emap@netcompany.com>
2019-02-21 15:32:40 +0100

    add bootstrap-grid

--- RoutePlanningCES/RoutePlanningCES/App_Start/BundleConfig.cs
+++ RoutePlanningCES/RoutePlanningCES/App_Start/BundleConfig.cs
@@ -24,6 +24,7 @@ namespace RoutePlanningCES
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-grid.css",
                       "~/Content/site.css"));
         }
     }

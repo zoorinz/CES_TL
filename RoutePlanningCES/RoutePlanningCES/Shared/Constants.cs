ab10c56a87a9d265bd5b416230d9bae0c8fd6916
Christian Engelbrecht Mikkelsen <cemi@netcompany.com>
2019-02-21 11:09:23 +0100

    Added Shared.Constants plus corrections to calculator

---
+++ RoutePlanningCES/RoutePlanningCES/Shared/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+
+namespace RoutePlanningCES.Shared
+{
+    public class Constants
+    {
+        //Prices
+        public int SegmentPrice = 3;
+        public int RecommendedPrice = 10;
+        public double LiveAnimalsPrice = 0.50;
+        public double CautiousParcelsPrice = 0.75;
+        public double RefrigeratedGoodsPrice = 0.10;
+    }
+}

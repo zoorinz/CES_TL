14c8ee18dcf409acdb49081e39a23e5ac4fc7e7f
Irina Alina Gabriela Luca <iagl@netcompany.com>
2019-02-21 15:34:31 +0100

    Add some header stying

--- RoutePlanningCES/RoutePlanningCES/App_Start/BundleConfig.cs
+++ RoutePlanningCES/RoutePlanningCES/App_Start/BundleConfig.cs
@@ -24,7 +24,8 @@ namespace RoutePlanningCES
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/Styling/nav.css"));
         }
     }
 }

6c82bef7c3db02ade239dae34ef7f107f44ca9ff
Martin Haaber <marth@netcompany.com>
2019-02-21 13:11:19 +0100

    addede the last models

--- RoutePlanningCES/RoutePlanningCES/Models/Type.cs
+++ RoutePlanningCES/RoutePlanningCES/Models/Type.cs
@@ -1,7 +1,10 @@
-﻿namespace Models
+﻿using System.Web.Services.Description;
+
+namespace Models
 {
     public class Type
     {
-        
+        public int ID { get; set; }
+        public  string name { get; set; }
     }
 }
